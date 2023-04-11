78d60c6195715a8afdcae1ef400716009dd96089
laurence <kkupop3@yahoo.com>
2023-04-11 17:50:44 +0200

    feat:added time

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import './App.css';
 import {useState,useEffect} from "react"
 import WeatherUiToday from './components/weatherui';
-import {Inputbar } from './components/inputbar';
+import {Inputbar} from './components/inputbar';
 
 // import { eventProp } from './components/inputbar';
 
@@ -13,57 +13,66 @@ tomorrow.setDate(tomorrow.getDate() + 1);
 
 function App() {
 // let [value,setValue] = useState("")
-let [lat,setLat] = useState("")  // get the latitude of the text in the input bar
-let [lon,setLon] = useState("")  // get the longitude of the text in the input bar
-let [loading,setLoading] = useState(false)
+let [lat,setLat] = useState<string>("") // get the latitude of the text in the input bar
+let [lon,setLon] = useState<string>("")  // get the longitude of the text in the input bar
+let [loading,setLoading] = useState<boolean>(false)
 // Todays Weather info
-let [city,setCity] = useState("")
-let [country,setCountry] = useState("")
-let [date,setDate] = useState("")
-let [time, setTime] = useState("")
-let [weatherDescription,setWeatherDescription] = useState("") // data.weather.description
-let [temperature,setTemperature] = useState("")
-let [main,setMain] = useState("")
-let [humidity, setHumidity] = useState("")
-let [windSpeed,setWindSpeed] = useState("")
-let [icon, setIcon] = useState("")
-let [fetchSuccess, setFetchSuccess] = useState(false)
+let [city,setCity] = useState<string>("")
+let [country,setCountry] = useState<string>("")
+let [date,setDate] = useState<string>("")
+let [time, setTime] = useState<string>("")
+let [weatherDescription,setWeatherDescription] = useState<string>("") // data.weather.description
+let [temperature,setTemperature] = useState<string>("")
+let [main,setMain] = useState<string>("")
+let [humidity, setHumidity] =useState<string>("")
+let [windSpeed,setWindSpeed] = useState<string>("")
+let [icon, setIcon] = useState<string>("")
+let [fetchSuccess, setFetchSuccess] = useState<boolean>(false)
 
 // Tomorrows Weather Info
-let [next_Weather,setNext_Weather] = useState("")  // set tomorrows main weather
-let [tomorrowIcon,setTomorrowIcon] = useState("")
-let [tomorrowTemp,setTomorrowTemp] = useState("")
-let [tomorrowWeatherDesc,setTomorrowWeatherDesc] = useState("")
+let [next_Weather,setNext_Weather] = useState<string>("")  // set tomorrows main weather
+let [tomorrowIcon,setTomorrowIcon] = useState<string>("")
+let [tomorrowTemp,setTomorrowTemp] = useState<string>("")
+let [tomorrowWeatherDesc,setTomorrowWeatherDesc] = useState<string>("")
 
 // 2 days Weather Info
-let [twoDayWeather,setTwoDayWeather] = useState("") 
-let [twoDayIcon,setTwoDayIcon] = useState("")
-let [twoDayTemp,setTwoDayTemp] = useState("")
-let [twoDayWeatherDesc,setTwoDayWeatherDesc] = useState("")
+let [twoDayWeather,setTwoDayWeather] = useState<string>("") 
+let [twoDayIcon,setTwoDayIcon] = useState<string>("")
+let [twoDayTemp,setTwoDayTemp] = useState<string>("")
+let [twoDayWeatherDesc,setTwoDayWeatherDesc] = useState<string>("")
 
 // 3 days Weather Info
-let [threeDayWeather,setThreeDayWeather] = useState("")  
-let [threeDayIcon,setThreeDayIcon] = useState("")
-let [threeDayTemp,setThreeDayTemp] = useState("")
-let [threeDayWeatherDesc,setThreeDayWeatherDesc] = useState("")
+let [threeDayWeather,setThreeDayWeather] = useState<string>("") 
+let [threeDayIcon,setThreeDayIcon] = useState<string>("")
+let [threeDayTemp,setThreeDayTemp] = useState<string>("")
+let [threeDayWeatherDesc,setThreeDayWeatherDesc] = useState<string>("")
 
 // 4 days Weather info
-let [fourDayWeather,setFourDayWeather] = useState("")  
-let [fourDayIcon,setFourDayIcon] = useState("")
-let [fourDayTemp,setFourDayTemp] = useState("")
-let [fourDayWeatherDesc,setFourDayWeatherDesc] = useState("")
+let [fourDayWeather,setFourDayWeather] = useState<string>("") 
+let [fourDayIcon,setFourDayIcon] = useState<string>("")
+let [fourDayTemp,setFourDayTemp] = useState<string>("")
+let [fourDayWeatherDesc,setFourDayWeatherDesc] = useState<string>("")
 
 //5 days Weather info
-let [fiveDayWeather,setFiveDayWeather] = useState("")  
-let [fiveDayIcon,setFiveDayIcon] = useState("")
-let [fiveDayTemp,setFiveDayTemp] = useState("")
-let [fiveDayWeatherDesc,setFiveDayWeatherDesc] = useState("")
-let [fiveDayWeatherDesctwo,setFiveDayWeatherDesctwo] = useState("")
+let [fiveDayWeather,setFiveDayWeather] = useState<string>("") 
+let [fiveDayIcon,setFiveDayIcon] = useState<string>("")
+let [fiveDayTemp,setFiveDayTemp] = useState<string>("")
+let [fiveDayWeatherDesc,setFiveDayWeatherDesc] = useState<string>("")
+let [fiveDayWeatherDesctwo,setFiveDayWeatherDesctwo] = useState<string>("")
 
+/* remove date */  
+let [removeDate,setRemoveDate] = useState<boolean>(false)
+
+/* after 3 seconds */
+let [removeanim,setremoveanim] = useState<boolean>(false)
+
+
+let getdate = new Date()
+console.log(getdate.toLocaleDateString("default", {day:"numeric", month:"long", year:"numeric"}))
   return (
     <div className="App">
-
-      <Inputbar setFiveDayWeatherDesctwo={setFiveDayWeatherDesctwo} setFiveDayWeather={setFiveDayWeather} setFiveDayIcon={setFiveDayIcon} setFiveDayTemp={setFiveDayTemp} setFiveDayWeatherDesc={setFiveDayWeatherDesc} setFourDayIcon={setFourDayIcon} setFourDayWeather={setFourDayWeather} setFourDayTemp={setFourDayTemp} setFourDayWeatherDesc={setFourDayWeatherDesc} setWindSpeed={setWindSpeed} setThreeDayWeatherDesc={setThreeDayWeatherDesc} setTwoDayWeatherDesc={setTwoDayWeatherDesc} setTomorrowWeatherDesc={setTomorrowWeatherDesc}setLat={setLat} setLon={setLon} setWeatherDescription={setWeatherDescription} setTime={setTime} setDate={setDate} setCountry={setCountry} setCity={setCity} setTemperature={setTemperature} setMain={setMain} setHumidity={setHumidity} setLoading={setLoading} setNext_Weather={setNext_Weather} setIcon={setIcon} setTomorrowIcon={setTomorrowIcon} setTomorrowTemp={setTomorrowTemp} setTwoDayIcon={setTwoDayIcon} setTwoDayTemp={setTwoDayTemp} setTwoDayWeather={setTwoDayWeather} setThreeDayIcon={setThreeDayIcon} setThreeDayTemp={setThreeDayTemp} setThreeDayWeather={setThreeDayWeather}  setFetchSuccess={setFetchSuccess}/> 
+ { removeDate==false? <div className="header"> <h1 className="cssanimation leFadeIn random headerwords"> {getdate.toLocaleDateString("default", {weekday:"long", day:"numeric", month:"long", year:"numeric"})} </h1></div> :  ""}
+      <Inputbar setRemoveDate={setRemoveDate} setFiveDayWeatherDesctwo={setFiveDayWeatherDesctwo} setFiveDayWeather={setFiveDayWeather} setFiveDayIcon={setFiveDayIcon} setFiveDayTemp={setFiveDayTemp} setFiveDayWeatherDesc={setFiveDayWeatherDesc} setFourDayIcon={setFourDayIcon} setFourDayWeather={setFourDayWeather} setFourDayTemp={setFourDayTemp} setFourDayWeatherDesc={setFourDayWeatherDesc} setWindSpeed={setWindSpeed} setThreeDayWeatherDesc={setThreeDayWeatherDesc} setTwoDayWeatherDesc={setTwoDayWeatherDesc} setTomorrowWeatherDesc={setTomorrowWeatherDesc}setLat={setLat} setLon={setLon} setWeatherDescription={setWeatherDescription} setTime={setTime} setDate={setDate} setCountry={setCountry} setCity={setCity} setTemperature={setTemperature} setMain={setMain} setHumidity={setHumidity} setLoading={setLoading} setNext_Weather={setNext_Weather} setIcon={setIcon} setTomorrowIcon={setTomorrowIcon} setTomorrowTemp={setTomorrowTemp} setTwoDayIcon={setTwoDayIcon} setTwoDayTemp={setTwoDayTemp} setTwoDayWeather={setTwoDayWeather} setThreeDayIcon={setThreeDayIcon} setThreeDayTemp={setThreeDayTemp} setThreeDayWeather={setThreeDayWeather}  setFetchSuccess={setFetchSuccess}/> 
 
       <WeatherUiToday   fiveDayWeatherDesctwo={fiveDayWeatherDesctwo} setFiveDayWeatherDesc={setFiveDayWeatherDesc} fourDayWeather={fourDayWeather} fourDayIcon={fourDayIcon} fourDayTemp={fourDayTemp} fourDayWeatherDesc={fourDayWeatherDesc} fiveDayWeather={fiveDayWeather} fiveDayIcon={fiveDayIcon} fiveDayWeatherDesc={fiveDayWeatherDesc} fiveDayTemp={fiveDayTemp}  humidity={humidity} windSpeed={windSpeed} fetchSuccess={fetchSuccess} tomorrowWeatherDesc={tomorrowWeatherDesc} twoDayWeatherDesc={twoDayWeatherDesc} threeDayWeatherDesc={threeDayWeatherDesc} lat={lat} lon={lon} city={city} country={country} weatherDescription={weatherDescription} temperature={temperature} main={main} loading={loading} next_Weather={next_Weather} icon={icon} tomorrowIcon={tomorrowIcon} tomorrowTemp={tomorrowTemp} twoDayIcon={twoDayIcon} twoDayTemp={twoDayTemp} twoDayWeather={twoDayWeather} threeDayIcon={threeDayIcon} threeDayTemp={threeDayTemp} threeDayWeather={threeDayWeather}/>
 
